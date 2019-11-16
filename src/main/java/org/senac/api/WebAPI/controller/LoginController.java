cb6123696d356747ea745795413dc6d6fa188d8e
Christian Adriano <chradr@gmail.com>
2019-11-16 16:24:47 -0300

    liberando autenticação

--- src/main/java/org/senac/api/WebAPI/controller/LoginController.java
+++ src/main/java/org/senac/api/WebAPI/controller/LoginController.java
@@ -19,27 +19,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
-*
-* @author Christian
-*/
+ *
+ * @author Christian
+ */
 
 @RestController
 @RequestMapping("/login")
-@CrossOrigin(origins="*")
+@CrossOrigin(origins = "*")
 public class LoginController {
 	@Autowired
 	private LoginRepository repository;
-	
+
 	@GetMapping
 	public List<Login> findAll() {
 		return repository.findAll();
 	}
-	
+
 	@PostMapping
 	public Login salvar(@RequestBody @Valid Login login) {
 		return repository.save(login);
 	}
-	
+
 	@GetMapping("/{id}")
 	public ResponseEntity<?> find(@PathVariable("id") Integer id) {
 		Optional<Login> login = repository.findById(id);
@@ -48,10 +48,29 @@ public class LoginController {
 		}
 		return ResponseEntity.notFound().build();
 	}
-	
+
+	@PostMapping("/autentica")
+	public String validaLogin(@RequestBody Login login) {
+		String retorno = "erro!";
+		List<Login> lista = repository.findAll();
+		for (Login l : lista) {
+			if (l.getLogin().equalsIgnoreCase(login.getLogin())) {
+				if (l.getSenha().equals(login.getSenha())) {
+					retorno = "sucesso!";
+				} else {
+					retorno = "senha inválida!";
+				}
+				break;
+			} else {
+				retorno = "login Inválido!";
+			}
+		}
+		return retorno;
+	}
+
 	@DeleteMapping("/{id}")
 	public void delete(@PathVariable("id") Integer id) {
 		repository.deleteById(id);
-	}	
+	}
 
 }
--- src/main/java/org/senac/api/WebAPI/repository/LoginRepository.java
+++ src/main/java/org/senac/api/WebAPI/repository/LoginRepository.java
@@ -9,5 +9,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 */
 
 public interface LoginRepository extends JpaRepository<Login, Integer>{
-
+	
 }
